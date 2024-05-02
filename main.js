34f1877736be4fd8fb56afdaaaa1fc5145ce55de
zwaxi <muhammadishkuvatov@gmail.com>
2024-05-02 14:57:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let a = ["Mandarin", "Banan", "Malina", "Shaftoli"];
+let b = a.length;
+alert("Arriynig uzunligi " + b);
+let t ;
+console.log(a)
+let d =  confirm("Sizning array malumotlarni olib tashlamoqchimisiz");
+if (d) {
+    t = a.pop()
+}
+
+console.log();
+console.log(a);
+
+
